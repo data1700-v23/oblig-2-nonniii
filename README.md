2c2d1f28c615ae4c0f5840e740d5f790f60b7beb
nonniii <113233864+nonniii@users.noreply.github.com>
2023-03-18 20:08:49 +0100

    Opprettelse av Spring boot-projekt og oppdatering av README fil.

--- README.md
+++ README.md
@@ -8,4 +8,15 @@ Github repo URL: https://github.com/data1700-v23/oblig-2-nonniii
 
 Fullt navn: Nonni Gravdahl
 
-Kort beskrivelse av applikasjon (5-10 setninger):
+Denne applikasjonen er en nettside for bestilling og
+administrasjon av kinobilletter. 
+Brukere kan bestille billetter ved å fylle ut 
+et skjema, og billettene blir deretter lagret 
+i en database. 
+Administratoren kan se en oversikt over
+alle billetter som er bestilt, og har 
+muligheten til å slette billetter. 
+*En feil i koden her: klarer ikke
+å lagre bestillingen. 
+
+
--- src/main/java/obli2/webprog/BillettController.java
+++ src/main/java/obli2/webprog/BillettController.java
@@ -14,15 +14,25 @@ public class BillettController {
     @Autowired
     BillettRepository repository;
 
+    /*Lagre: tar imot en Kinobilletter-objekt og legger den til i databasen vha.
+    BillettRepository-objektet.
+     */
     @PostMapping("/bestill")
     public void lagre(@RequestBody Kinobilletter billett) {
         repository.leggTilBillett(billett);
     }
-
+    /*
+    Henter alle kinobillettene fra databasen vha. BillettRepository og returnerer
+    dem som en liste.
+     */
     @GetMapping("/hent")
     public List<Kinobilletter> hent() {
         return repository.hentAlleBilletter();
     }
+
+    /*
+    Sletter alle kinobillettene fra databasen vha. BillettRepository.
+     */
 
     @DeleteMapping("/slett")
     public void slett() {
